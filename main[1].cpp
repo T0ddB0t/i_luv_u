fdfb65c3473508aa51476075cfe9531112c2088e
T0ddB0t <93291262+T0ddB0t@users.noreply.github.com>
2022-02-17 09:52:50 -0500

    Update main[1].cpp

--- main[1].cpp
+++ main[1].cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main() 
 {
-  vector<string> ANS = {"i luv u", "ok \ni still luv u"};
+  vector<string> ANS = {"i luv u", "ok \ni still luv u", "well ok \nu don't know \ni still luv u"};
   
   vector<string> QUS = {"how u feel:\n", "bad", "ok"};
   int x;
@@ -20,7 +20,10 @@ int main()
     cout << ANS[0];
   }else if(x == 2){
     cout << ANS[1];   
+  }else{
+    cout << ANS[2];
   }
+    
   
   
 }
