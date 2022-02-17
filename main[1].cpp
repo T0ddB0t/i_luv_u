cddd9b47ecea350cd47af4136cef9c7560a6b5c5
T0ddB0t <93291262+T0ddB0t@users.noreply.github.com>
2022-02-16 21:41:27 -0500

    Add files via upload

---
+++ main[1].cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() 
+{
+  vector<string> ANS = {"i luv u", "ok \ni still luv u"};
+  
+  vector<string> QUS = {"how u feel:\n", "bad", "ok"};
+  int x;
+  cout << QUS[0];
+  for(int i = 1; i < 3; i++)
+    {
+      cout << i << ": " << QUS[i] << endl;
+    }
+  cin >> x;
+  if(x == 1)
+  {
+    cout << ANS[0];
+  }else if(x == 2){
+    cout << ANS[1];   
+  }
+  
+  
+}
